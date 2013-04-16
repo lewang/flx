6189f05c7e45688f0e83e28941bf6b6914b75b95
Le Wang <le.wang@agworld.com.au>
2013-04-17 01:00:10 +0800

    add flx, ido implementation and helm experiment

---
+++ flx-ido.el
@@ -0,0 +1,73 @@
+;;;
+;;; credit to scott frazer's blog entry here:http://scottfrazersblog.blogspot.com.au/2009/12/emacs-better-ido-flex-matching.html
+;;;
+
+(require 'ido)
+(require 'flx)
+
+;;; dynamically bound by ido
+(defvar hist)
+
+(defun flx-ido-match (query items)
+  "Better sorting for flx ido matching."
+  (if (zerop (length query))
+      items
+    (let ((cache (if (eq hist 'ido-file-history)
+                     flx-file-cache
+                   flx-strings-cache))
+          matches)
+      (mapc (lambda (item)
+              (let ((score (flx-score item query cache)))
+                (when score
+                  (push (cons item (car score)) matches))))
+            items)
+      (mapcar 'car (if ido-rotate
+                       matches
+                     (sort matches (lambda (x y) (> (cdr x) (cdr y)))))))))
+
+(defvar flx-ido-use t
+  "*Use flx matching for ido.")
+
+(defadvice ido-set-matches-1 (around flx-ido-set-matches-1 activate)
+  "Choose between the regular ido-set-matches-1 and my-ido-fuzzy-match"
+  (if flx-ido-use
+      (setq ad-return-value (flx-ido-match ido-text (ad-get-arg 0)))
+    ad-do-it))
+
+
+(setq ido-enable-flex-matching t)
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;; testing
+
+;; (defvar ido-enable-replace-completing-read t
+;;   "If t, use ido-completing-read instead of completing-read if possible.
+
+;;     Set it to nil using let in around-advice for functions where the
+;;     original completing-read is required.  For example, if a function
+;;     foo absolutely must use the original completing-read, define some
+;;     advice like this:
+
+;;     (defadvice foo (around original-completing-read-only activate)
+;;       (let (ido-enable-replace-completing-read) ad-do-it))")
+
+;; ;; Replace completing-read wherever possible, unless directed otherwise
+;; (defadvice completing-read
+;;   (around use-ido-when-possible activate)
+;;   (if (or (not ido-enable-replace-completing-read) ; Manual override disable ido
+;;           (and (boundp 'ido-cur-list)
+;;                ido-cur-list)) ; Avoid infinite loop from ido calling this
+;;       ad-do-it
+;;     (let ((allcomp (all-completions "" collection predicate)))
+;;       (if allcomp
+;;           (setq ad-return-value
+;;                 (ido-completing-read prompt
+;;                                      allcomp
+;;                                      nil require-match initial-input hist def))
+;;         ad-do-it))))
+
+;; (ido-everywhere t)
+
+(provide 'flx-ido)
---
+++ flx-scratch-helm.el
@@ -0,0 +1,69 @@
+(require 'flx)
+(require 'flx-test-list)
+
+(defun helm-mp-flx-propertize (str score)
+  "Return propertized string according to score."
+  (let ((block-started (cadr score))
+        (last-char nil))
+    (loop for char in (cdr score)
+          do (progn
+               (when (and last-char
+                          (not (= (1+ last-char) char)))
+                 (put-text-property block-started  (1+ last-char) 'face 'helm-match str)
+                 (setq block-started char))
+               (setq last-char char)))
+    (put-text-property block-started  (1+ last-char) 'face 'helm-match str)
+    (format "% [%s]" str (car score))))
+
+(defun flx-helm-candidate-transformer (candidates)
+  "We score candidate and add the score info for later use.
+
+The score info we add here is later removed with another filter."
+  (if (zerop (length helm-pattern))
+      candidates
+    (let* ((mp-3-patterns (helm-mp-3-get-patterns helm-pattern))
+           (flx-pattern (cdar mp-3-patterns))
+           (patterns (cons (cons 'identity
+                                 (mapconcat
+                                  #'identity
+                                  (split-string flx-pattern "" t)
+                                  ".*"))
+                           (cdr mp-3-patterns)))
+           res)
+      (setq res (loop for candidate in candidates
+                      for matched = (loop for (predicate . regexp) in patterns
+                                          always (funcall predicate (string-match regexp (helm-candidate-get-display candidate))))
+                      if matched
+                      collect (let ((score (flx-score candidate flx-pattern flx-file-cache)))
+                                (unless (consp candidate)
+                                  (setq candidate (cons (copy-sequence candidate) candidate)))
+                                (setcdr candidate (cons (cdr candidate) score))
+                                candidate)))
+      (sort res
+            (lambda (a b)
+              (> (caddr a) (caddr b))))
+      (loop for item in res
+            for index from 0
+            for score = (cddr item)
+            do (progn
+                 ;; highlight first 20 matches
+                 (when (and (< index 20) (> (car score) 0))
+                   (setcar item (helm-mp-flx-propertize (car item) score)))
+                 (setcdr item (cadr item))))
+      res)))
+
+(defun flx-helm-test-candidates ()
+  foo-list)
+
+(setq flx-helm-candidate-list-test
+      '((name . "flx candidate-list-test")
+        (candidates . flx-helm-test-candidates)
+        (candidate-transformer flx-helm-candidate-transformer)
+        (volatile)
+        (match-strict identity)
+        ))
+
+
+(defun flx-helm-demo ()
+  (interactive)
+  (helm :sources '(flx-helm-candidate-list-test)))
